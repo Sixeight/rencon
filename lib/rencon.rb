ccd9748b43d5be85ffa8f0f660a765f37b133994
Tomohiro Nishimura <tomohiro68@gmail.com>
2009-07-31 14:29:07 +0900

    Fix: Rencon became a module, and add Core class
      * Rencon class is now Core class
      * All code is wraped by Rencon module

--- lib/rencon.rb
+++ lib/rencon.rb
@@ -3,64 +3,67 @@ require 'mechanize'
 require 'kconv'
 require 'csv'
 
-class Rencon
+module Rencon
 
-  attr_reader :states
+  class Core
 
-  def initialize(config)
-    check(config) or raise 'Invalid configuration'
-    @config = config
-    @states  = nil
-    @agent  = WWW::Mechanize.new
-    @agent.user_agent = 'Mac Safari'
-    login
-  end
+    attr_reader :states
 
-  def retrieve(project, per_page = 25)
-    title = @agent.get("http://#{conf(:host)}/projects/show/#{project}").title.sub(/\s-.*\z/, '')
-
-    per_page = conf(:per_page) || per_page
-    @agent.get "http://#{conf(:host)}/projects/#{project}/issues/?per_page=#{per_page}&format=csv"
-    @states, *tickets  = CSV.parse(@agent.page.body.to_s.toutf8).
-      # FIXME: Muiltiline descriptions are rounded off to one lines.
-      #        #reject can not know correct number of issue's status.
-      #        now size < 10, but this is a first aid. need fix rapidly.
-      reject {|issue| issue.size < 10 }
-
-    [tickets, title]
-  end
-
-  def lambda_mark
-    mine = conf(:mark, :mine) || '+'
-    none = conf(:mark, :none) || '-'
-    lambda {|name| /#{conf(:name)}/ =~ name ? mine : none }
-  end
-
-  def mark(name)
-    lambda_mark[name]
-  end
-
-  private
-  def login
-    @agent.get "http://#{conf(:host)}/login"
-    @agent.page.form_with(:action => '/login') do |f|
-      f.field_with(:name => 'username').value = conf(:user)
-      f.field_with(:name => 'password').value = conf(:pass)
-      f.click_button
+    def initialize(config)
+      check(config) or raise 'Invalid configuration'
+      @config = config
+      @states  = nil
+      @agent  = WWW::Mechanize.new
+      @agent.user_agent = 'Mac Safari'
+      login
     end
-  end
 
-  # FIXME: need more check
-  def check(config)
-    [ :host,
-      :user,
-      :pass,
-      :name,
-    ].all? {|key| config.has_key? key }
-  end
+    def retrieve(project, per_page = 25)
+      title = @agent.get("http://#{conf(:host)}/projects/show/#{project}").title.sub(/\s-.*\z/, '')
 
-  def conf(*keys)
-    keys.inject(@config, :[]) rescue nil
+      per_page = conf(:per_page) || per_page
+      @agent.get "http://#{conf(:host)}/projects/#{project}/issues/?per_page=#{per_page}&format=csv"
+      @states, *tickets  = CSV.parse(@agent.page.body.to_s.toutf8).
+        # FIXME: Muiltiline descriptions are rounded off to one lines.
+        #        #reject can not know correct number of issue's status.
+        #        now size < 10, but this is a first aid. need fix rapidly.
+        reject {|issue| issue.size < 10 }
+
+      [tickets, title]
+    end
+
+    def lambda_mark
+      mine = conf(:mark, :mine) || '+'
+      none = conf(:mark, :none) || '-'
+      lambda {|name| /#{conf(:name)}/ =~ name ? mine : none }
+    end
+
+    def mark(name)
+      lambda_mark[name]
+    end
+
+    private
+    def login
+      @agent.get "http://#{conf(:host)}/login"
+      @agent.page.form_with(:action => '/login') do |f|
+        f.field_with(:name => 'username').value = conf(:user)
+        f.field_with(:name => 'password').value = conf(:pass)
+        f.click_button
+      end
+    end
+
+    # FIXME: need more check
+    def check(config)
+      [ :host,
+        :user,
+        :pass,
+        :name,
+      ].all? {|key| config.has_key? key }
+    end
+
+    def conf(*keys)
+      keys.inject(@config, :[]) rescue nil
+    end
   end
 end
 
--- spec/rencon_spec.rb
+++ spec/rencon_spec.rb
@@ -1,103 +1,108 @@
 require File.dirname(__FILE__) + '/../lib/rencon.rb'
 
-describe Rencon do
-  before do
-    class Rencon
-      def initialize(config)
-        @config = config
-      end
-    end
-  end
+module Rencon
 
-  it 'check method should check the configuration' do
-    rencon = Rencon.new(nil)
+  describe Core do
 
-    config = {}
-    [:host, :user, :pass, :name].each do |key|
-      rencon.__send__(:check, config).should be_false
-      config.store key, nil
-    end
-    rencon.__send__(:check, config).should be_true
-  end
-
-  it 'conf method should be able to parse a doubled hash and return the correct value' do
-    @foo = 'foo'
-    @bar = 'bar'
-    rencon = Rencon.new({:one => {
-                           :two1 => @foo,
-                           :two2 => @bar
-                        }})
-    rencon.__send__(:conf, :one, :two1).should == @foo
-    rencon.__send__(:conf, :one, :two2).should == @bar
-    rencon.__send__(:conf, :one, :two3).should == nil
-  end
-
-  describe 'specification around the mark method' do
     before do
-      @correct_name = 'John Rencon'
-      @wrong_name   = 'Tomohiro Nishimura'
-      @rencon = Rencon.new({:name => @correct_name})
-    end
-
-    it 'should make lambda returns mark' do
-      mark = @rencon.lambda_mark
-      mark.should be_kind_of Proc
-      mark[@correct_name].should == '+'
-      mark[@wrong_name].should == '-'
-    end
-
-    it 'can set another mark word' do
-      rencon = Rencon.new({:name => @correct_name,
-                           :mark => {
-                             :mine => '*',
-                             :none => 'x',
-                           }})
-      mark = rencon.lambda_mark
-      mark[@correct_name].should == '*'
-      mark[@wrong_name].should == 'x'
-    end
-
-    it 'should mark method makes icon by name' do
-      @rencon.mark(@correct_name).should == '+'
-      @rencon.mark(@wrong_name).should == '-'
-    end
-  end
-
-  describe 'when call retrieve method' do
-    before do
-      @page  = mock('page', :null_object => true)
-      @agent = mock('agent')
-      @agent.stub!(:get => @page, :page => @page)
-      class Rencon
-        # login can't success on offline test
+      class Core
         def initialize(config)
-          @config = Hash.new
-          @agent = config
+          @config = config
         end
       end
-      @rencon = Rencon.new @agent
-
-      @states = mock('names')
-      @tickets = mock('tickets')
-      @page.stub!(:body)
-      @title = mock('title')
-      @title.stub!(:sub => @title)
     end
 
-    it 'should retrieve tickets and title' do
-      @tickets.stub!(:reject => [@states, @tickets] )
-      @page.should_receive(:title).and_return(@title)
-      CSV.should_receive(:parse).and_return(@tickets)
+    it 'check method should check the configuration' do
+      rencon = Core.new(nil)
 
-      @rencon.retrieve('rencon').should == [[@tickets], @title]
+      config = {}
+      [:host, :user, :pass, :name].each do |key|
+        rencon.__send__(:check, config).should be_false
+        config.store key, nil
+      end
+      rencon.__send__(:check, config).should be_true
     end
 
-    it 'should states methods returns ticket states name' do
-      CSV.stub!(:parse => @tickets)
-      @states = ['#', 'tracker', 'user']
-      @tickets.stub!(:reject => [@names, @tickets] )
-      @rencon.retrieve('rencon')
-      @rencon.states.should == @names
+    it 'conf method should be able to parse a doubled hash and return the correct value' do
+      @foo = 'foo'
+      @bar = 'bar'
+      rencon = Core.new({:one => {
+                             :two1 => @foo,
+                             :two2 => @bar
+                          }})
+      rencon.__send__(:conf, :one, :two1).should == @foo
+      rencon.__send__(:conf, :one, :two2).should == @bar
+      rencon.__send__(:conf, :one, :two3).should == nil
+    end
+
+    describe 'specification around the mark method' do
+      before do
+        @correct_name = 'John Rencon'
+        @wrong_name   = 'Tomohiro Nishimura'
+        @rencon = Core.new({:name => @correct_name})
+      end
+
+      it 'should make lambda returns mark' do
+        mark = @rencon.lambda_mark
+        mark.should be_kind_of Proc
+        mark[@correct_name].should == '+'
+        mark[@wrong_name].should == '-'
+      end
+
+      it 'can set another mark word' do
+        rencon = Core.new({:name => @correct_name,
+                             :mark => {
+                               :mine => '*',
+                               :none => 'x',
+                             }})
+        mark = rencon.lambda_mark
+        mark[@correct_name].should == '*'
+        mark[@wrong_name].should == 'x'
+      end
+
+      it 'should mark method makes icon by name' do
+        @rencon.mark(@correct_name).should == '+'
+        @rencon.mark(@wrong_name).should == '-'
+      end
+    end
+
+    describe 'when call retrieve method' do
+      before do
+        @page  = mock('page', :null_object => true)
+        @agent = mock('agent')
+        @agent.stub!(:get => @page, :page => @page)
+        class Core
+          # login can't success on offline test
+          def initialize(config)
+            @config = Hash.new
+            @agent = config
+          end
+        end
+        @rencon = Core.new @agent
+
+        @states = mock('names')
+        @tickets = mock('tickets')
+        @page.stub!(:body)
+        @title = mock('title')
+        @title.stub!(:sub => @title)
+      end
+
+      it 'should retrieve tickets and title' do
+        @tickets.stub!(:reject => [@states, @tickets] )
+        @page.should_receive(:title).and_return(@title)
+        CSV.should_receive(:parse).and_return(@tickets)
+
+        @rencon.retrieve('rencon').should == [[@tickets], @title]
+      end
+
+      it 'should states methods returns ticket states name' do
+        CSV.stub!(:parse => @tickets)
+        @states = ['#', 'tracker', 'user']
+        @tickets.stub!(:reject => [@names, @tickets] )
+        @rencon.retrieve('rencon')
+        @rencon.states.should == @names
+      end
     end
   end
 end
+
